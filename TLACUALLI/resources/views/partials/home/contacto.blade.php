8dff0e6eaadf2d3ef0ef1b16a14a2ed8f7bd23e2
AdiGalvan <133288217+AdiGalvan@users.noreply.github.com>
2024-07-27 16:36:15 -0600

    Vista Home
    Carrusel:
    Se cambiaron las imágenes y se les agregará una pequeña frase para que no no sólo estén las imágenes. Falta definir color, estilo de fuente y las frases el texto que llevará. Ya se encuentra con Tailwind.
    
    MisiónVisión:
    Se probó un diseño en cards, pero se contempla cambiar el diseño. Ya se encuentra en tailwind.
    
    Sección publicaciones:
    Se agregó un fondo amarillo. Se cuenta con un carrusel para ver las publicaciones y un botón que llevará a la vista "publicaciones". Ya se encuentra en tailwind.
    
    Preguntas frecuentes:
    Ya se encuentra en tailwind pero aún falta definir su estilo. Se agregaron 5 preguntas más.
    
    Contacto:
    Ya se encuentra en Tailwind.
    
    Aliados:
    Aún no se encuentra en Tailwind

---
+++ TLACUALLI/resources/views/partials/home/contacto.blade.php
@@ -0,0 +1,72 @@
+
+
+
+<!-- <div class="container">
+    <a class="flex flex-col items-center bg-black border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+        <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-96 md:rounded-none md:rounded-s-lg" src="https://pixabay.com/get/g10234e829ce7a21d292d3cd8f5fe62ff66d70cedd57a7b434916235f46b336aeafb013822721dc716483cc6eee18cd38a699b0ca999a8358c8a9f94a366cf588_1280.jpg" alt="">
+        <div class="flex flex-col justify-between p-4 leading-normal">
+            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+            <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        </div>
+    </a>
+</div> -->
+
+<div class="grid grid-cols-2 gap-4 p-3 w-screen bg-yellow-100 border border-gray-300 shadow-md">
+  <div class="w-screen">
+    <img class="object-cover w-full h-96" src="/images/contacto.jpg" alt="">
+  </div> 
+  <div class="max-w-xl">
+  <div class="col-span-2 ">
+    <div class="w-full text-start">
+      <h2 class="text-4xl font-black text-yellow-500 dark:text-white font-sans">Contáctanos</h2>
+    </div>
+  </div>
+    <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">¡Estaremos felices de atenderte!</h5>
+    <span class="mb-3 font-normal text-lg text-black">Horario de atención: lunes a viernes de 09:00 a.m a 17:00 p.m.</span>
+    <p class="mb-3 font-normal text-lg text-black">Respondemos en un plazo máximo de 24 horas.</p>
+    <div class="grid grid-cols-2 gap-4 mt-5">
+      <span class="flex justify-start">
+        <svg class="w-12 h-12 text-4xl text-green-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18.427 14.768 17.2 13.542a1.733 1.733 0 0 0-2.45 0l-.613.613a1.732 1.732 0 0 1-2.45 0l-1.838-1.84a1.735 1.735 0 0 1 0-2.452l.612-.613a1.735 1.735 0 0 0 0-2.452L9.237 5.572a1.6 1.6 0 0 0-2.45 0c-3.223 3.2-1.702 6.896 1.519 10.117 3.22 3.221 6.914 4.745 10.12 1.535a1.601 1.601 0 0 0 0-2.456Z"/>
+        </svg>
+        <div class="">
+        <p class="text-lg font-extrabold font-sans">Teléfono</p>
+        <p class="text-lg ">576 895 1023</p>
+        </div>
+      </span>
+      <span class="flex justify-start">
+        <svg class="w-12 h-12 text-4xl text-green-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path fill="currentColor" fill-rule="evenodd" d="M12 4a8 8 0 0 0-6.895 12.06l.569.718-.697 2.359 2.32-.648.379.243A8 8 0 1 0 12 4ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10a9.96 9.96 0 0 1-5.016-1.347l-4.948 1.382 1.426-4.829-.006-.007-.033-.055A9.958 9.958 0 0 1 2 12Z" clip-rule="evenodd"/>
+          <path fill="currentColor" d="M16.735 13.492c-.038-.018-1.497-.736-1.756-.83a1.008 1.008 0 0 0-.34-.075c-.196 0-.362.098-.49.291-.146.217-.587.732-.723.886-.018.02-.042.045-.057.045-.013 0-.239-.093-.307-.123-1.564-.68-2.751-2.313-2.914-2.589-.023-.04-.024-.057-.024-.057.005-.021.058-.074.085-.101.08-.079.166-.182.249-.283l.117-.14c.121-.14.175-.25.237-.375l.033-.066a.68.68 0 0 0-.02-.64c-.034-.069-.65-1.555-.715-1.711-.158-.377-.366-.552-.655-.552-.027 0 0 0-.112.005-.137.005-.883.104-1.213.311-.35.22-.94.924-.94 2.16 0 1.112.705 2.162 1.008 2.561l.041.06c1.161 1.695 2.608 2.951 4.074 3.537 1.412.564 2.081.63 2.461.63.16 0 .288-.013.4-.024l.072-.007c.488-.043 1.56-.599 1.804-1.276.192-.534.243-1.117.115-1.329-.088-.144-.239-.216-.43-.308Z"/>
+        </svg>
+        <div class="">
+        <p class="text-lg font-extrabold font-sans">WhatsApp</p>
+        <p class="text-lg ">647 927 2031</p>
+        </div>
+      </span>
+      <span class="flex justify-start">
+        <svg class="w-12 h-12 text-4xl text-green-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+          <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z"/>
+        </svg>
+        <div class="">
+        <p class="text-lg font-extrabold font-sans">E-mail</p>
+        <p class="text-lg ">tlacualli@gmail.com.mx</p>
+        </div>
+      </span>
+      <span class="flex justify-start">
+        <svg class="w-12 h-12 text-4xl text-green-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+          <path fill-rule="evenodd" d="M13.135 6H15V3h-1.865a4.147 4.147 0 0 0-4.142 4.142V9H7v3h2v9.938h3V12h2.021l.592-3H12V6.591A.6.6 0 0 1 12.592 6h.543Z" clip-rule="evenodd"/>
+        </svg>
+        <div class="">
+        <p class="text-lg font-extrabold font-sans">Facebook</p>
+        <p class="text-lg ">Tlacualli-Oficial</p>
+        </div>
+      </span> 
+    </div>
+  </div>
+</div>
+
+
+
+
+
